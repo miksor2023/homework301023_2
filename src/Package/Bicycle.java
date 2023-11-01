3d647dc64f061151cb623402b18ec50f3642785f
miksor2023 <miksor@yandex.ru>
2023-11-01 11:58:50 +0300

    initial commit

---
+++ src/Package/Bicycle.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
